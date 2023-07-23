fd606848e35609feee6b510e1324739a18e806b7
Azury Industries <83734987+AzuryIndustry@users.noreply.github.com>
2023-07-23 16:51:57 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,32 @@
 console.log("[!] Property of Azury Industries")
+
+if(typeof document !== 'undefined') {
+    window.onscroll = function() {scrollFunction()};
+    function scrollFunction() {
+        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+            if(!document.querySelector(".nav-items").animate) {
+            document.querySelector(".nav-items").animate([
+                { transform: 'translateY(-100%)' },
+                { transform: 'translateY(0)' }
+            ], {
+                duration: 500,
+                easing: 'ease-in-out',
+                fill: 'forwards'
+            });
+            }
+            document.querySelector(".nav-items").style.display = "flex";
+            document.querySelector(".nav-items").style.position = "fixed";
+        } else {
+            document.querySelector(".nav-items").animate([
+                { transform: 'translateY(-200%)' },
+                { transform: 'translateY(-100%)' }
+            ], {
+                duration: 500,
+                easing: 'ease-in-out',
+                fill: 'forwards'
+            });
+            document.querySelector(".nav-items").style.display = "none";
+            document.querySelector(".nav-items").style.position = "relative";
+        }
+    }
+}
