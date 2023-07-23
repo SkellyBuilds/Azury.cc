5fab630f19e2a8b454f57c6f1951cec3a644c3a7
Azury Industries <83734987+AzuryIndustry@users.noreply.github.com>
2023-07-23 19:06:40 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 
+import toast, { Toaster } from 'react-hot-toast';
 export default function Index() {
     let thisDate = Date.now();
     const countDown = (start: number, end: number) => {
@@ -10,20 +11,68 @@ export default function Index() {
         
         return `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
     };
+
+    let text = ['Studios', 'Community', 'Hangout', 'Development'];
+    let usedText = [];
+
+    setInterval(() => {
+        if(typeof document !== 'undefined') {
+            const randomText = () => {
+                const random = Math.floor(Math.random() * text.length);
+                if(usedText.length === text.length) {
+                    usedText = [];
+                    document.querySelector('.main-text').innerHTML = 'Industries'
+                }
+                if(usedText.includes(text[random])) {
+                    randomText();
+                } else {
+                    usedText.push(text[random]);
+                    document.querySelector('.main-text').innerHTML = text[random];
+                }
+            };
+            randomText();
+        }
+    }, 10000);
+
+    const notify = () => {
+        if(Notification.permission === 'granted') {
+            toast.error('You have already been set to be notified!');
+        } else {
+            Notification.requestPermission().then((permission) => {
+                if(permission === 'granted') {
+                    localStorage.setItem('notified', 'true');
+                    toast.success('You will be notified in '+countDown(thisDate, new Date('2023-10-08').getTime()))
+                } else {
+                    toast.error('You have denied notifications!')
+                }
+            });
+        }
+    }
+
+    const sendNotification = (title: string, body: string) => {
+        if(typeof document !== 'undefined') {
+        if(Notification?.permission === 'granted') {
+            new Notification(title, { body });
+        } 
+    }
+    }
     
     return (
         setInterval(() => {
             thisDate = Date.now();
             typeof document !== 'undefined' ? document.querySelector('.cointdown-text').innerHTML = countDown(thisDate, new Date('2023-10-08').getTime()) : null;
+            if(thisDate === new Date('2023-10-08-00:00:00').getTime()) {
+                sendNotification('Azury Industries', 'Azury Industries is now available! Check it out at https://azury.cc');
+            }
         }, 1000),
         <>
         <div className="h-96 flex items-center justify-center">
             <div>
-                <h1 className="font-bold text-white text-6xl text-center">Azury Industries</h1>
+                <h1 className="flex items-center justify-center font-bold text-6xl text-center text-white">Azury&nbsp;<p className="main-text">Industries</p></h1>
                 <p className="mt-3 font-light text-center mx-auto text-zinc-300">COMING SOON IN <span className="cointdown-text">{countDown(thisDate, new Date('2023-10-08').getTime())}</span></p>
-                <a target="_blank" href="https://dsc.gg/azuryindustry" className="bg-blue-400 hover:bg-white shadow-blue-800/20 hover:shadow-white/20 shadow-xl py-3 px-6 mx-auto mt-5 flex items-center justify-center rounded-lg text-zinc-900 transition-all">
-                    <i className="mr-1.5 fab fa-github" /> Join our Discord
-                </a>
+                <button onClick={notify} className="bg-blue-400 hover:bg-white shadow-blue-800/20 hover:shadow-white/20 shadow-xl py-3 px-6 mx-auto mt-5 flex items-center justify-center rounded-lg text-zinc-900 transition-all">
+                    <i className="mr-1.5 fa fa-bell" /> Notify me
+                </button>
             </div>
         </div>
         <div className="flex flex-col space-y-5 text-left w-80">
@@ -61,7 +110,7 @@ export default function Index() {
             </a>
 
             <a className="team-card" target="_blank" href="https://discord.com/users/1090632016812326954">
-                <img src="" className="rounded-full w-10 h-10 mx-auto" />
+                <img src="https://cdn.discordapp.com/avatars/1090632016812326954/d7cebc7b04ddc0848d28ca3723ee9f69.webp" className="rounded-full w-10 h-10 mx-auto" />
                 <div className="team-username">
                 <h2 className="font-bold text-white text-2xl text-center">Mee69</h2>
                 </div>
