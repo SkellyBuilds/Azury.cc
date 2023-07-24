bda04bea500bf2a88eefb85f8ab17636fb0b3bb2
Azury Industries <83734987+AzuryIndustry@users.noreply.github.com>
2023-07-23 21:26:39 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,22 +4,12 @@ if(typeof document !== 'undefined') {
     window.onscroll = function() {scrollFunction()};
     function scrollFunction() {
         if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-            if(!document.querySelector(".nav-items").animate) {
-            document.querySelector(".nav-items").animate([
-                { transform: 'translateY(-100%)' },
-                { transform: 'translateY(0)' }
-            ], {
-                duration: 500,
-                easing: 'ease-in-out',
-                fill: 'forwards'
-            });
-            }
             document.querySelector(".nav-items").style.display = "flex";
             document.querySelector(".nav-items").style.position = "fixed";
         } else {
             document.querySelector(".nav-items").animate([
-                { transform: 'translateY(-200%)' },
-                { transform: 'translateY(-100%)' }
+                { transform: 'translateY(-300%)' },
+                { transform: 'translateY(-120%)' }
             ], {
                 duration: 500,
                 easing: 'ease-in-out',
